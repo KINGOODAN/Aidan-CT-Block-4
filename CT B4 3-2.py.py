a091cebaecf6e86ecd7c6f7dd6bb959511693358
KINGOODAN <77458991+KINGOODAN@users.noreply.github.com>
2021-03-09 13:22:14 -0700

    Add files via upload

---
+++ CT B4 3-2.py.py
@@ -0,0 +1,10 @@
+def responce():
+    while True:
+        res = input('say Hi:  ')
+        if res == 'Hi':
+            print ('Hi')
+            break
+        elif res == 'Hello':
+            print ('Hello')
+            break
+responce()
