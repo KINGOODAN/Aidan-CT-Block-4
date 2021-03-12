2972d698623f72bb2b259a5635615729fa55e715
KINGOODAN <77458991+KINGOODAN@users.noreply.github.com>
2021-03-12 13:12:18 -0700

    Add files via upload

--- CT B4 3-9.py
+++ CT B4 3-9.py
@@ -6,20 +6,26 @@ Program Goals:
     a. Add more values to a list
     b. Return a value at a specific index
 '''
+import random
 myList = []
 def mainProgram():
     while True:
-        print("Hello, there! Let's work with lists!")
-        print("Choose from the following options. Type a number below!")
-        choice = input("""1. Add to a list
-    2. Return the value at an index position
-    3. ExitProgram     """)
-        if choice == "1":
-            addToList()
-        elif choice == "2":
-            indexValues()
-        elif choice == "3":
-            break
+        try:
+            print("Hello, there! Let's work with lists!")
+            print("Choose from the following options. Type a number below!")
+            choice = input("""1. Add to a list
+2. Return the value at an index position
+3. ExitProgram     """)
+            if choice == "1":
+                addToList()
+            elif choice == "2":
+                indexValues()
+            elif choice == "3":
+                randomSearch()
+            else:
+                break
+        except:
+            print("You made a whoopsie!")
     #TO ADD: 1. A way to loop the action, 2. A way to quit, 3.Think of repetition.
 
 def addToList():
@@ -30,7 +36,11 @@ def addToList():
 def indexValues():
     print ("Ohhh! I eard you need a particular piece of data")
     indexPos = input("What index position are you curious about?     ")
-    print(myList[int(indexPos)])
+    print(myList[int(indexPos)-1])
+
+def randomSearch():
+    print("RaNdOm SeArCh?!?")
+    print(myList[random.randint(0, len(myList)-1)])
 
 
 if __name__ == "__main__":
