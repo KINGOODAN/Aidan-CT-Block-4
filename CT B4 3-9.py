15460168a09b87590bfc582ad407006bba503649
KINGOODAN <77458991+KINGOODAN@users.noreply.github.com>
2021-03-18 13:28:07 -0600

    Add files via upload

--- CT B4 3-9.py
+++ CT B4 3-9.py
@@ -1,11 +1,12 @@
 '''
 Program Goals:
-1. Get input from the user (at multiple points)
-2. We need to convert some of this input to INTs from STRs
-3. We need to provide choices to the user 
-    a. Add more values to a list
-    b. Return a value at a specific index
+    1. Get input from the user (at multiple points)
+    2. We need to convert some of this input to INTs from STRs
+    3. We need to provide choices to the user 
+        a. Add more values to a list
+        b. Return a value at a specific index
 '''
+
 import random
 myList = []
 def mainProgram():
@@ -13,16 +14,22 @@ def mainProgram():
         try:
             print("Hello, there! Let's work with lists!")
             print("Choose from the following options. Type a number below!")
-            choice = input("""1. Add to a list
-2. Return the value at an index position
-3. Random Search 
-4. ExitProgram     """)
+            choice = input("""  1. Add to a list
+    2. Add a bunch of numbers
+    3. Return the value at an index position
+    4. Random Search 
+    5. Print contents of list
+    6. ExitProgram     """)
             if choice == "1":
                 addToList()
             elif choice == "2":
-                indexValues()
+                addABunch()
             elif choice == "3":
+                indexValues()
+            elif choice == "4":
                 randomSearch()
+            elif choice == "5":
+                print(myList)
             else:
                 break
         except:
@@ -34,6 +41,14 @@ def addToList():
     newItem = input ("Type an integer here!     ")
     myList.append(int(newItem))
 
+def addABunch():
+    print ("We're gonna add a bunch of integers here!")
+    numToAdd = input("How many new integers would you like to add?     ")
+    numRange = input("And how high would you like these numbers to go?     ")
+    for myList in range(0, int(numToAdd)):
+        myList.append(random.randint(0,int(numRange)))
+    print("Your list is a new complete.")
+
 def indexValues():
     print ("Ohhh! I eard you need a particular piece of data")
     indexPos = input("What index position are you curious about?     ")
@@ -43,6 +58,12 @@ def randomSearch():
     print("RaNdOm SeArCh?!?")
     print(myList[random.randint(0, len(myList)-1)])
 
+def linearSearch():
+    print("We're gonna check out each ietm ona at a time in your list! This sucks.")
+    searchItem = input("What you lookin for, pardner?     ")
+    for x in range(len(myList)):
+        if myList [x] == (searchItem):
+            print (f"Your item is at index position {x}")
 
 if __name__ == "__main__":
     mainProgram()
