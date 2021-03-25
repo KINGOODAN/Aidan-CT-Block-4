92bc77fe86fbf57f351753e9c43f066b22291c87
KINGOODAN <77458991+KINGOODAN@users.noreply.github.com>
2021-03-25 13:25:10 -0600

    Add files via upload

--- CT B4 3-9.py
+++ CT B4 3-9.py
@@ -6,22 +6,25 @@ Program Goals:
         a. Add more values to a list
         b. Return a value at a specific index
 '''
-
+#----------
 import random
 myList = []
+unique_list =[]
+#----------
 def mainProgram():
     while True:
         try:
             print("Hello, there! Let's work with lists!")
             print("Choose from the following options. Type a number below!")
-            choice = input("""  1. Add to a list
+            choice = input("""    1. Add to a list
     2. Add a bunch of numbers
     3. Add a few
     4. Return the value at an index position
     5. Random Search 
     6. Linear Search
-    7. Print contents of list
-    8. ExitProgram
+    7. Sort list
+    8. Print lists
+    9. ExitProgram
      -- """)
             if choice == "1":
                 addToList()
@@ -36,49 +39,70 @@ def mainProgram():
             elif choice == "6":
                 linearSearch()
             elif choice == "7":
-                print(myList)
+                sortList(myList)
+            elif choice == "8":
+                printLists()
             else:
                 break
         except:
             print("You made a whoopsie!")
-    #TO ADD: 1. A way to loop the action, 2. A way to quit, 3.Think of repetition.
-
+#----------
 def addToList():
     print ("Adding to a list! Great choice!")
     newItem = input ("Type an integer here!" + "\n -- ")
     myList.append(int(newItem))
-
+#----------
 def addABunch():
     print ("We're gonna add a bunch of integers here!")
     numToAdd = input("How many new integers would you like to add?" + "\n -- ")
     numRange = input("And how high would you like these numbers to go?" + "\n -- ")
-    for myList in range(0, int(numToAdd)):
-        myList.append(random.randint(0,int(numRange)))
+    for x in range(0, int(numToAdd)):
+        myList.append(random.randint(0, int(numRange)))
     print("Your list is a new complete.")
-
+#----------
 def addAFew():
+    print("It seem that you want to add a few numbers!")
     while True:
-        print("It seem that you want to add a few numbers!" + "\n -- ")
         whatNum = input("What numbers would you like to add if you are done type stop." + "\n -- ")
         myList.append(int(whatNum))
         if whatNum == ("stop"):
             return
-
+#----------
 def indexValues():
     print ("Ohhh! I eard you need a particular piece of data")
     indexPos = input("What index position are you curious about?" + "\n -- ")
-    print(myList[int(indexPos)-1])
-
+    print(myList[int(indexPos)])
+#----------
+def sortList (myList):
+    print("A litte birdie told me you needed some sorted data!")
+    for x in myList:
+        if x not in unique_list:
+            unique_list.append(x)
+    unique_list.sort()
+    showMe = input('Wanna see your new list?   Y/N' + "\n -- ")
+    if showMe.lower() == 'y':
+        print(unique_list)
+#----------
 def randomSearch():
     print("RaNdOm SeArCh?!?")
     print(myList[random.randint(0, len(myList)-1)])
-
+#----------
 def linearSearch():
     print("We're gonna check out each ietm ona at a time in your list! This sucks.")
     searchItem = input("What you lookin for, pardner?" + "\n -- ")
-    for x in range(len(myList)):
-        if myList [x] == (searchItem):
+    for x in range (len(myList)):
+        if myList[x] == int(searchItem):
             print (f"Your item is at index position {x}")
-
+#----------
+def printLists():
+    if len(unique_list) == 0:
+        print(myList)
+    else:
+        whichOne = input("Which list? Sorted or unsrted?" + "\n -- ")
+        if whichOne.lower() == "sorted":
+            print(unique_list)
+        else:
+            print(myList)
+#----------
 if __name__ == "__main__":
     mainProgram()
